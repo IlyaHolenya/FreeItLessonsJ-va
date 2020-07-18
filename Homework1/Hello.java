4b6702aeb70dbe51a548ee6fd135ab1f6ea9628f
IlyaHolenya <67831668+IlyaHolenya@users.noreply.github.com>
2020-07-18 10:29:55 +0300

    Homework1

--- Homework1/Hello.java
+++ Homework1/Hello.java
@@ -1,5 +1,8 @@
 class Hello {
-    public static void main(String[] args) {
+	// javac Hello.java	
+	// java Hello
+	    public static void main(String[] args) {
         System.out.println("Hello " + args [3]);
+	// java Hello y g lk Ilya
     }
 }
